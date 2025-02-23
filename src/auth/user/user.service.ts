04dbcfcc1b2fdc22086b090033ad45c083b73494
hhshams <hhshams1333@gmail.com>
2025-02-23 20:08:13 +0330

    Implemented UserService for user registration and retrieval

--- src/auth/user/user.service.ts
+++ src/auth/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ) { }
+
+    async create(username: string, password: string): Promise<User> {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = this.userRepository.create({ username, password: hashedPassword });
+        return this.userRepository.save(user);
+    }
+
+    async findOne(username: string): Promise<User | null> {
+        return this.userRepository.findOneBy({ username });
+    }
+}
