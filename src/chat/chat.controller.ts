cacaef3a98b989bb803508042bdfe8fe13bb3a4b
hhshams <hhshams1333@gmail.com>
2025-02-23 20:19:27 +0330

    Updated ChatController to use DTOs for validation in chat room and message handling

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Post, Body, Get, Param, UseGuards } from '@nestjs/common';
 import { ChatService } from './chat.service';
-import { ChatRoom } from 'src/chat/chat-room.entity';
-import { Message } from 'src/chat/message.entity';
+import { ChatRoom } from './chat-room.entity';
+import { Message } from './message.entity';
+import { CreateChatRoomDto } from './create-chat-room.dto';
+import { SendMessageDto } from './send-message.dto';
+import { JoinChatRoomDto } from './join-chat-room.dto';
 import { JwtAuthGuard } from 'src/jwt.guard';
 
 @Controller('chat')
@@ -10,22 +13,24 @@ export class ChatController {
 
     @UseGuards(JwtAuthGuard)
     @Post('rooms')
-    async createChatRoom(@Body('name') name: string): Promise<ChatRoom> {
-        return await this.chatService.createChatRoom(name);
+    async createChatRoom(@Body() createChatRoomDto: CreateChatRoomDto): Promise<ChatRoom> {
+        return this.chatService.createChatRoom(createChatRoomDto.name);
     }
 
-    @Post('rooms/:id/join')
-    async joinChatRoom(@Param('id') id: number): Promise<ChatRoom | never> {
-        return this.chatService.joinChatRoom(id);
+    @UseGuards(JwtAuthGuard)
+    @Post('rooms/join')
+    async joinChatRoom(@Body() joinChatRoomDto: JoinChatRoomDto): Promise<ChatRoom> {
+        return this.chatService.joinChatRoom(joinChatRoomDto.id);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Post('rooms/:id/messages')
-    async sendMessage(@Param('id') id: number, @Body('content') content: string): Promise<Message> {
-        return await this.chatService.sendMessage(id, content);
+    async sendMessage(@Param('id') id: number, @Body() sendMessageDto: SendMessageDto): Promise<Message> {
+        return this.chatService.sendMessage(id, sendMessageDto.content);
     }
 
     @Get('rooms/:id/messages')
     async getMessages(@Param('id') id: number): Promise<Message[]> {
-        return await this.chatService.getMessages(id);
+        return this.chatService.getMessages(id);
     }
 }
