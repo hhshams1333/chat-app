c9e319bc94e857085d31e7660d33578be8120816
hhshams <hhshams1333@gmail.com>
2025-02-23 20:22:23 +0330

    Added Helmet middleware for setting HTTP security headers

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true })); // فعال‌سازی اعتبارسنجی
+
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
