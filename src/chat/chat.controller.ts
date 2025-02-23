12170b1ac0c2ede9ebbac15fcf6496ccddeed194
hhshams <hhshams1333@gmail.com>
2025-02-23 19:57:21 +0330

    Implemented ChatService and ChatController with CRUD operations

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { ChatRoom } from 'src/entity/chat-room.entity';
+import { Message } from 'src/entity/message.entity';
 
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+    constructor(private chatService: ChatService) { }
+
+    @Post('rooms')
+    async createChatRoom(@Body('name') name: string): Promise<ChatRoom> {
+        return await this.chatService.createChatRoom(name);
+    }
+
+    @Post('rooms/:id/join')
+    async joinChatRoom(@Param('id') id: number): Promise<ChatRoom | never> {
+        return this.chatService.joinChatRoom(id);
+    }
+
+    @Post('rooms/:id/messages')
+    async sendMessage(@Param('id') id: number, @Body('content') content: string): Promise<Message> {
+        return await this.chatService.sendMessage(id, content);
+    }
+
+    @Get('rooms/:id/messages')
+    async getMessages(@Param('id') id: number): Promise<Message[]> {
+        return await this.chatService.getMessages(id);
+    }
+}
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChatRoom } from 'src/entity/chat-room.entity';
+import { Message } from 'src/entity/message.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+    constructor(
+        @InjectRepository(ChatRoom)
+        private chatRoomRepository: Repository<ChatRoom>,
+        @InjectRepository(Message)
+        private messageRepository: Repository<Message>,
+    ) { }
+
+    async createChatRoom(name: string): Promise<ChatRoom> {
+        const chatRoom = this.chatRoomRepository.create({ name });
+        return this.chatRoomRepository.save(chatRoom);
+    }
+
+    async joinChatRoom(id: number): Promise<ChatRoom | never> {
+        const chatRoom = await this.chatRoomRepository.findOneBy({ id });
+        if (chatRoom)
+            return chatRoom
+        else
+            throw new Error("Chat Room with the given ID Does not Exists")
+    }
+
+    async sendMessage(chatRoomId: number, content: string): Promise<Message> {
+        const message = this.messageRepository.create({ content });
+        const chatRoom = await this.chatRoomRepository.findOneBy({ id: chatRoomId });
+        if (message && chatRoom)
+            message.chatRoom = chatRoom
+        return this.messageRepository.save(message);
+
+    }
+
+    async getMessages(chatRoomId: number): Promise<Message[]> {
+        return this.messageRepository.find({ where: { chatRoom: { id: chatRoomId } } });
+    }
+}
