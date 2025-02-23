c8d351d8595ed27a0d0b11b755869579e9a8c682
hhshams <hhshams1333@gmail.com>
2025-02-23 20:30:38 +0330

    Configured Throttler module with rate limiting settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,15 @@ import { ChatRoom } from './chat/chat-room.entity';
 import { Message } from './chat/message.entity';
 import { ChatModule } from './chat/chat.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
