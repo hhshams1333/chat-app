b337f3b9378ba06d86642be702c7a5181e662181
hhshams <hhshams1333@gmail.com>
2025-02-23 20:15:41 +0330

    Updated AuthController to use DTOs for validation in registration and login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './create-user.dto';
+import { LoginUserDto } from './login-user.dto';
 import { User } from './user/user.entity';
 
 @Controller('auth')
@@ -7,12 +9,12 @@ export class AuthController {
     constructor(private authService: AuthService) { }
 
     @Post('register')
-    async register(@Body() body: { username: string; password: string }): Promise<User> {
-        return this.authService.register(body.username, body.password);
+    async register(@Body() createUserDto: CreateUserDto): Promise<User> {
+        return this.authService.register(createUserDto.username, createUserDto.password);
     }
 
     @Post('login')
-    async login(@Body() body: { username: string; password: string }): Promise<{ access_token: string }> {
-        return this.authService.login(body.username, body.password);
+    async login(@Body() loginUserDto: LoginUserDto): Promise<{ access_token: string }> {
+        return this.authService.login(loginUserDto.username, loginUserDto.password);
     }
 }
