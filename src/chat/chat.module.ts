d2e74f16fde18660133895d3363181724a43264c
hhshams <hhshams1333@gmail.com>
2025-02-23 20:57:11 +0330

    Import chatroom and message entity in chat room module

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { ChatController } from './chat.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatRoom } from './chat-room.entity';
+import { Message } from './message.entity';
+import { Repository } from 'typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ChatRoom, Message]), // وارد کردن Entity ها
+  ],
   controllers: [ChatController],
-  providers: [ChatService]
+  providers: [ChatService, Repository<ChatRoom>, Repository<Message>],
+  exports: [ChatService]
 })
-export class ChatModule {}
+export class ChatModule { }
